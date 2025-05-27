ce0ef1e423de7b964d989be3d6031e05b5b3b828
Gabriel gardin <gabrielgardin7@gmail.com>
2025-05-27 15:06:12 -0300

    objetos

---
+++ aula04/objetos.ts
@@ -0,0 +1,8 @@
+let objeto : object
+
+objeto = {nome: 'gabriel', idade: 18}
+
+
+let objetos : {nome: string, idade: number, ehLegal:boolean}
+
+objetos = {nome: 'gabriel', idade: 18, ehLegal: true}
