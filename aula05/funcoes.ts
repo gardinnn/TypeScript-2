13b892697003543dbd4296c4c70f16f2a08859d2
Gabriel gardin <gabrielgardin7@gmail.com>
2025-05-28 15:29:39 -0300

    Create funcoes.ts

---
+++ aula05/funcoes.ts
@@ -0,0 +1,11 @@
+function falar(): string{
+    return 'hello'
+}
+
+function soma(a:number, b:number): number{
+    return a + b
+}
+
+// AEROFUNCTION
+
+const soma2 = (a:number, b:number) => a + b 
